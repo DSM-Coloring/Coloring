6374071e969b50448e6faf129ab7be711b4d5364
서지유 <jihoseo2008@gmail.com>
2026-05-21 09:28:38 +0900

    feat :: home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
+import { InteractiveSection } from "./features/filter/ui/InteractiveSection"
+import { AboutSection } from "./features/home/ui/AboutSection"
+import { CallToAction } from "./features/home/ui/CallToAction"
+import { ColorBlindnessSection } from "./features/home/ui/ColorBlindnessSection"
+import { Footer } from "./features/home/ui/Footer"
+import { HeroSection } from "./features/home/ui/HeroSection"
+
 export default function Home() {
-    return <div>HelloWorld</div>
+    return (
+        <main className="w-full min-h-screen bg-zinc-50">
+            <HeroSection />
+            <AboutSection />
+            <ColorBlindnessSection />
+            <InteractiveSection />
+            <CallToAction />
+            <Footer />
+        </main>
+    )
 }
